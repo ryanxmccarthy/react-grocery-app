22bbf086043f15003eb55e8be3ad987f49968ab5
Ryan XVX <ryanxmccarthy@gmail.com>
2020-07-26 15:20:08 -0700

    setting up counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 1,
+  };
+
   render() {
+    let classes = this.getBadgeClasses();
+
     return (
       <div>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 

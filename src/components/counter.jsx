d9f6e36d4af2765297331f7830b885b609ff57ef
Ryan XVX <ryanxmccarthy@gmail.com>
2020-07-26 17:17:35 -0700

    adding button functionality and cleaning up state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.counter.value,
   };
 
-  //   constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  //   }
-
   handleIncrement = () => {
-    console.log("Increment Clicked", this);
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -24,19 +19,25 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
